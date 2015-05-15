bd64e8c655f5f51337ba72af742f87cd8ab9c2f7
Jan Wedekind (Dr) <jan@wedesoft.de>
2015-05-15 23:01:11 +0100

    Separate gem specification

---
+++ config.rb
@@ -0,0 +1,22 @@
+require 'rake'
+
+PKG_NAME = 'hornetseye-ffmpeg'
+PKG_VERSION = '1.2.2'
+CFG = RbConfig::CONFIG
+CXX = ENV[ 'CXX' ] || 'g++'
+RB_FILES = ['config.rb'] + FileList[ 'lib/**/*.rb' ]
+CC_FILES = FileList[ 'ext/*.cc' ]
+HH_FILES = FileList[ 'ext/*.hh' ] + FileList[ 'ext/*.tcc' ]
+TC_FILES = FileList[ 'test/tc_*.rb' ]
+TS_FILES = FileList[ 'test/ts_*.rb' ]
+SO_FILE = "ext/#{PKG_NAME.tr '\-', '_'}.#{CFG[ 'DLEXT' ]}"
+PKG_FILES = [ 'Rakefile', 'README.md', 'COPYING', '.document' ] +
+            RB_FILES + CC_FILES + HH_FILES + TS_FILES + TC_FILES
+BIN_FILES = [ 'README.md', 'COPYING', '.document', SO_FILE ] +
+            RB_FILES + TS_FILES + TC_FILES
+SUMMARY = %q{Read/write video frames using libffmpeg}
+DESCRIPTION = %q{This Ruby extension defines the class Hornetseye::AVInput for reading frames from video files and the class Hornetseye::AVOutput for writing frames to video files.}
+LICENSE = 'GPL-3+'
+AUTHOR = %q{Jan Wedekind}
+EMAIL = %q{jan@wedesoft.de}
+HOMEPAGE = %q{http://wedesoft.github.com/hornetseye-ffmpeg/}
---
+++ hornetseye-ffmpeg.gemspec
@@ -0,0 +1,24 @@
+require_relative 'config'
+
+Gem::Specification.new do |s|
+  s.name = PKG_NAME
+  s.version = PKG_VERSION
+  s.platform = Gem::Platform::RUBY
+  s.date = Date.today.to_s
+  s.summary = SUMMARY
+  s.description = DESCRIPTION
+  s.license = LICENSE
+  s.author = AUTHOR
+  s.email = EMAIL
+  s.homepage = HOMEPAGE
+  s.files = PKG_FILES
+  s.test_files = TC_FILES
+  s.require_paths = [ 'lib', 'ext' ]
+  s.rubyforge_project = %q{hornetseye}
+  s.extensions = %w{Rakefile}
+  s.has_rdoc = 'yard'
+  s.extra_rdoc_files = []
+  s.add_dependency %<malloc>, [ '~> 1.2' ]
+  s.add_dependency %<multiarray>, [ '~> 1.0' ]
+  s.add_dependency %<hornetseye-frame>, [ '~> 1.0' ]
+end
